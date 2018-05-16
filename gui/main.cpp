e0e6d5229e4ed4c646b9b5fa0a934c5ea1264b4f
Camille <pasdemail@qquepart.fr>
2018-05-17 00:05:16 +0200

    Intégration basique pour tester du gui dans l'ancien moteur de jeu.

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,3 +1,105 @@
+#include "Engine.h"
+#include <allegro5/allegro.h>
+#include <allegro5\allegro_primitives.h>
+#include <allegro5\allegro_font.h>
+#include <allegro5\allegro_ttf.h>
+#include "JackamikazGUI.h"
+
+class JmGui : public Engine::Graphic, public Engine::Input {
+public:
+	Engine & engine;
+
+	jmg::Base root;
+	jmg::WallPaper wp;
+	jmg::Window win;
+	jmg::Text text;
+	jmg::MoveableRectangle mr;
+
+	JmGui(Engine& e)
+		: engine(e)
+		, wp(al_map_rgb(200, 200, 200))
+		, win(200, 250, "Salut les gens")
+		, text(u"Salut je teste ma vie\ngenre lol ààààààéééééé ouais trop bien tavu ouech genre vazi quoi")
+		, mr(200, 350)
+	{
+		engine.overlayGraphic.AddChild(this);
+		engine.inputRoot.AddChild(this);
+
+		mr.addChild(&text);
+		root.addChild(&mr);
+
+		win.mColor.g = 0;
+		win.mOutline = 1;
+		win.mRelx = 300;
+		win.mRely = 150;
+
+		text.mRelx = 10;
+		text.mRely = 10;
+		text.mWidth = mr.mWidth - text.mRelx * 2;
+
+		win.setParent(&root, false);
+	}
+
+	void Draw() {
+		root.needsRedraw();
+		root.baseDraw();
+	}
+
+	void Event(ALLEGRO_EVENT& event) {
+		if (event.type == ALLEGRO_EVENT_KEY_UP
+		 && event.keyboard.keycode == ALLEGRO_KEY_F1) {
+			win.open();
+		}
+		root.baseHandleEvent(event);
+	}
+};
+
+class FPSCounter : public Engine::Graphic {
+public:
+	Engine & engine;
+
+	double samplingDuration;
+	double lastTime;
+	int nbSamples;
+	int lastFps;
+
+	FPSCounter(Engine& e) : engine(e) {
+		engine.overlayGraphic.AddChild(this);
+
+		samplingDuration = 0.5;
+		lastTime = engine.time;
+		nbSamples = 0;
+		lastFps = (int)(1.0 / engine.dtTarget);
+	}
+
+	void Draw() {
+
+		++nbSamples;
+		if (engine.time - lastTime > samplingDuration) {
+			lastFps = (int)((double)nbSamples / samplingDuration);
+			nbSamples = 0;
+			lastTime = engine.time;
+		}
+
+		al_draw_textf(jmg::fetchDefaultFont(), al_map_rgb(255, 255, 255), 10, 10, 0, "FPS : %i", lastFps);
+	}
+};
+
+int main(int nbarg, char ** args) {
+	Engine engine;
+
+	if (engine.Init()) {
+		al_init_primitives_addon();
+		al_init_ttf_addon();
+
+		FPSCounter fc(engine);
+		JmGui gui(engine);
+
+		while (engine.OneLoop()) {}
+	}
+}
+
+/*
 #include <iostream>
 #include <allegro5\allegro.h>
 #include <allegro5\allegro_primitives.h>
@@ -69,3 +171,4 @@ int main(int nbarg, char ** args)
 
 	return 0;
 }
+*/
