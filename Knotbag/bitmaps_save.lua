467f9a391ac4eb8b7f59c9c9f9c15b63b7ee796e
Jackamilio <camille.bartalan@gmail.com>
2022-05-21 19:06:14 +0200

    Grabbed a file to save table to files

---
+++ Knotbag/bitmaps_save.lua
@@ -0,0 +1,37 @@
+return {
+-- Table: {1}
+{
+   {2},
+   {3},
+   {4},
+   ["banana"]={5},
+   ["errortexture"]={6},
+   ["__editor_values__"]={7},
+},
+-- Table: {2}
+{
+   ["file"]="errortexture.png",
+   ["name"]="errortexture",
+},
+-- Table: {3}
+{
+   ["file"]="banana.png",
+   ["name"]="banana",
+},
+-- Table: {4}
+{
+   ["file"]="cutemonkey.png",
+   ["name"]="monkey",
+},
+-- Table: {5}
+{
+},
+-- Table: {6}
+{
+},
+-- Table: {7}
+{
+   ["current"]=2,
+   ["elemwidth"]=100,
+},
+}
